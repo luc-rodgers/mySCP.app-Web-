1301ac2d355ed71b58f506fcf5d707169932e81a
luc-rodgers <rodgers.luc@gmail.com>
2026-04-25 19:54:13 -0700

    Revert "Add branded dark splash screen for app loading states"
    This reverts commit a7bb93abc046d1f85e262de2639c7df4e6664626.

--- app/loading.tsx
+++ app/loading.tsx
@@ -1,5 +1,7 @@
-import { AppLoading } from "@/components/AppLoading";
-
 export default function Loading() {
-  return <AppLoading />;
+  return (
+    <div className="flex items-center justify-center min-h-screen" style={{ backgroundColor: '#f3f3f5' }}>
+      <div className="w-10 h-10 border-4 border-gray-200 border-t-[#030213] rounded-full animate-spin" />
+    </div>
+  );
 }
